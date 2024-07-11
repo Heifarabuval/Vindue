a06721c59005d84ebc198e14b0659041625fbe5f
heifara <hbuval@adfinitas.fr>
2024-07-11 10:50:38 +0200

    ✨set services

--- src/lib/services/configuration.ts
+++ src/lib/services/configuration.ts
@@ -1,6 +1,12 @@
 import type { Configuration } from '../models/Configuration';
 import { createService } from './factory';
+import { projectService } from './project';
 
 export const configurationService = {
 	...createService<Configuration>('configurations'),
+	createConfiguration(configuration: Configuration,projectId:string) {
+		this.createDocument(configuration).then(async (docSnap:any) => {
+			await projectService.addProjectConfiguration(projectId, docSnap.id);
+		})
+	}
 };
--- src/lib/services/project.ts
+++ src/lib/services/project.ts
@@ -1,16 +1,32 @@
 import type { Project } from '../models/Project';
 import { createService } from './factory';
 import { companyService } from './company';
+import { doc } from 'firebase/firestore';
+import { firestore } from '../utils/db';
 
 export const projectService = {
 	...createService<Project>('projects'),
-	async getProjectConfigurations(companyId: string) {
-		console.log(`Getting configurations for company ${companyId}`);
+	createProject(project: Project, companyName: string) {
+		this.createDocument(project).then(async (docSnap: any) => {
+			await companyService.addProjectToCompany(companyName, docSnap.id);
+		});
 	},
-	createProject(project: Project,companyName:string) {
-		 this.createDocument(project).then((docSnap) => {
-			 companyService.addProjectToCompany(companyName, docSnap.id);
-		 })
-
+	async addProjectConfiguration(projectId: string, configurationId: string) {
+		const docRefConfiguration = doc(firestore, 'configurations', configurationId);
+		const projectConfigurations = await this.getDocument(projectId).then((doc) => {
+			return doc?.configurations || [] as any;
+		});
+		await this.updateDocument(projectId, { configurations: [...projectConfigurations, docRefConfiguration] });
+	},
+	async removeProjectConfiguration(projectId: string, configurationId: string) {
+		const docRefConfiguration = doc(firestore, 'configurations', configurationId);
+		const projectConfigurations = await this.getDocument(projectId).then((doc) => {
+			return doc?.configurations || [] as any;
+		});
+		await this.updateDocument(projectId, { configurations: projectConfigurations.filter((configuration: any) => configuration.id !== docRefConfiguration.id) });
+	},
+	async getProjectConfigurations(projectId: string) {
+		const doc = await this.getDocument(projectId);
+		return doc?.configurations || [] as any;
 	}
 };
