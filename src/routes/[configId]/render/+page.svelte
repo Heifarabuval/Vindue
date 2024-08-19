c1cc7dec5f3f4411e8453f50e5a878bf9f4855b7
Heifarabuval <heifara.buval@gmail.com>
2024-08-19 10:55:26 +0200

    ✨calculate position, size and se bg color

--- src/routes/[configId]/render/+page.svelte
+++ src/routes/[configId]/render/+page.svelte
@@ -12,12 +12,47 @@
 	onMount(() => {
 		if ($page.params.configId) {
 			incrementCounter($page.params.configId, 'views');
-		}});
+		}
+	});
 
+	const calculateMargin = (align: string): string => {
+		switch (align) {
+			case 'top':
+				return '0 auto auto auto';
+			case 'bottom':
+				return 'auto auto 0 auto';
+			case 'left':
+				return 'auto auto auto 0';
+			case 'right':
+				return 'auto 0 auto auto';
+			case 'center':
+				return 'auto';
+			default:
+				return 'auto';
+		}
+	};
 </script>
 
+<style>
+    .dynamic-background {
+        background-color: var(--background-color);
+    }
+
+		.dynamic-height {
+			height: var(--height);
+		}
+
+		.dynamic-width {
+			width: var(--width);
+		}
+
+		.dynamic-margin {
+			margin: var(--margin);
+		}
+</style>
+
 <Doc ref="configurations/{$page.params.configId}" let:data={configuration}>
-	<div style={"background-color:"+configuration.color} class="h-[100vh] bg-primary-500 p-3">
+	<div class="dynamic-background dynamic-height dynamic-width dynamic-margin" style="--background-color: {configuration.mainSettings.backgroundColor}; --height: {configuration.mainSettings.height}vh; --width: {configuration.mainSettings.width}vw; --margin: {calculateMargin(configuration.mainSettings.alignment)}">
 		<button on:click|preventDefault={handleClose} id="close-modal" class="bg-primary-300 text-white p-2 rounded-md">
 			<Doc ref="counter/{$page.params.configId}" let:data={counter}>
 				{counter.views}
